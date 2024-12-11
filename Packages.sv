2e8d43938f8c89330b13529a1718a1ed97f964a4
Moulya Raju Machohalli Thimmaraju <moulya@mo.ece.pdx.edu>
2024-12-10 16:18:03 -0800

    package push

--- Packages.sv
+++ Packages.sv
@@ -16,7 +16,7 @@ typedef enum { GETLINE, SENDLINE, INVALIDATELINE, EVICTLINE } message;
 
    typedef struct {
         logic [TAG_BITS-1:0] tag;  // Tag field
-         mesistate [1:0] MESI_BITS=I;
+         mesistate  MESI_BITS=I;
     } cache_block_t;
 
  typedef struct { 
